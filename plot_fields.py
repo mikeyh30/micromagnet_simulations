de615177c513a78a6a5fad5c69b7ab8e161a2a5d
Michael <mikey.hynes@hotmail.co.uk>
2021-01-25 11:53:08 +0000

    Large improvements to plotting
    Units implemented
    CNT is at right height
    Field and gradient are plotted, as well as slices for the CNT.

--- plot_fields.py
+++ plot_fields.py
@@ -1,40 +1,57 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
-from matplotlib.widgets import Slider
+from matplotlib import rcParams
 import argparse
+
+plt.rcParams["figure.figsize"] = (20, 10)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("a")
 args = parser.parse_args()
 
-f = np.load("B_field/"+args.a+".out/strayfield.npy")
+# Magnetostatic field (mT)
+f = np.load("B_field/" + args.a + ".out/strayfield.npy") * 1000
 
-dim, zr, yr, xr = f.shape                                                                  
+dim, zr, yr, xr = f.shape
 
-X,Y,Z = np.meshgrid(np.arange(0,128,1),np.arange(0,256,1),np.arange(4))  
-u,v,w = f[0,:,:,:],f[1,:,:,:],f[2,:,:,:]                                 
-u,v,w = u.transpose(1,2,0), v.transpose(1,2,0), w.transpose(1,2,0)
+X, Y, Z = np.meshgrid(np.arange(0, xr, 1), np.arange(0, yr, 1), np.arange(zr))
+u, v, w = f[0, :, :, :], f[1, :, :, :], f[2, :, :, :]
+u, v, w = u.transpose(1, 2, 0), v.transpose(1, 2, 0), w.transpose(1, 2, 0)
 
-fig, axes = plt.subplots(1,2)
+CNT_height = 15
+mag_B = np.hypot(u, v, w)
 
-f2 = np.load("B_field/"+args.a+".out/"+args.a+".npy")
+U, V, W = np.gradient(mag_B, 5, 5, 10)
 
-X2, Y2 = np.meshgrid(np.arange(0,128,1),np.arange(0,256,1))
+slice1 = 20
 
-a=3
+fig = plt.figure(constrained_layout=True)
+gs = fig.add_gridspec(4, 4)
+axes1 = fig.add_subplot(gs[:, 0])
+axes2 = fig.add_subplot(gs[:, 1])
+axes3 = fig.add_subplot(gs[:1, 2:])
+axes4 = fig.add_subplot(gs[2:, 2:])
 
-slice2 = 3
-mag_B = np.hypot(u[:,:,slice2],v[:,:,slice2])
+im1 = axes1.pcolor(np.abs(mag_B)[:, :, CNT_height])
+axes1.axvline(x=slice1, color="b", label="axvline - full height")
+cbar = fig.colorbar(im1, ax=axes1)
+axes1.set_title("Magnetostatic field (mT)")
 
-grad = np.gradient(mag_B)
+im2 = axes2.pcolor(np.abs(V)[:, :, CNT_height])
+axes2.axvline(x=slice1, color="b", label="axvline - full height")
+cbar = fig.colorbar(im2, ax=axes2)
+axes2.set_title("Magnetostatic field gradient (mT/nm)")
 
-slice1 = 39
+(l,) = axes3.plot(u[:, slice1, CNT_height], label="x")
+(l,) = axes3.plot(v[:, slice1, CNT_height], label="y")
+(l,) = axes3.plot(w[:, slice1, CNT_height], label="z")
+axes3.legend()
+axes3.set_title("Magnetostatic field (mT)")
 
-im3 = axes[1].imshow(grad[0])
-axes[1].axvline(x = slice1, color = 'b', label = 'axvline - full height') 
-cbar = fig.colorbar(im3, ax=axes[1])
-print(type(grad))
-l, = axes[0].plot(grad[0][:,slice1])
+(l,) = axes4.plot(U[:, slice1, CNT_height], label="x")
+(l,) = axes4.plot(V[:, slice1, CNT_height], label="y")
+(l,) = axes4.plot(W[:, slice1, CNT_height], label="z")
+axes4.legend()
+axes4.set_title("Magnetostatic field gradient (mT/nm)")
 
 plt.show()
